53620c5d230595e8354c9ff7c059a593de29b2c5
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-07-30 22:12:32 +0300

    Corrections done

--- src/js/Settings.js
+++ src/js/Settings.js
@@ -1,16 +1,33 @@
-class defSet {
-  constructor() {
-    this.arrSet = ['theme', 'dark', 'light', 'gray', 'music', 'trance', 'pop', 'rock', 'chillout', 'off', 'difficulty', 'easy', 'difficulty', 'normal', 'difficulty', 'hard', 'difficulty', 'nightmare'];
-    this.set = new Map([['theme', 'dark'], ['music', 'trance'], ['difficulty', 'easy']]);
+/* eslint-disable no-console */
+class Settings { // создаём класс
+  constructor(...option) { // опций может быть 0-3
+    option.forEach((item) => { // проверям какую именно опцию ввёл юзер
+      if (item === 'light' || item === 'dark' || item === 'grey') {
+        this.theme = item;
+      } else if (item === 'trance' || item === 'pop' || item === 'rock' || item === 'chillout' || item === 'off') {
+        this.music = item;
+      } else if (item === 'easy' || item === 'normal' || item === 'hard' || item === 'nightmare') {
+        this.difficulty = item;
+      }
+    });
   }
 
-  getUserSet(key, value) {
-    if (this.arrSet.includes(key) && this.arrSet.includes(value)) {
-      this.set.set(key, value);
-      return Array.from(this.set); // this.set.entries()
+  get getUserSet() {
+    const set = new Map([['theme', 'dark'], ['music', 'trance'], ['difficulty', 'easy']]); // создаём Мар с деф. настройками
+    if (this.theme) { // если опция существует, её ввёл юзер (не равна udefined)
+      set.set('theme', this.theme); // если нет, то будет дефолтная опция
     }
-    return 'Wrong data!';
+    if (this.music) { // тоже самое
+      set.set('music', this.music);
+    }
+    if (this.difficulty) { // тоже самое
+      set.set('difficulty', this.difficulty);
+    }
+    return Array.from(set); // возвращаем массив для теста
   }
 }
 
-export default defSet;
+const inst = new Settings('hard'); // строки для отладки в браузере
+console.log(inst.getUserSet);
+
+export default Settings;
--- src/js/__test__/Settings.test.js
+++ src/js/__test__/Settings.test.js
@@ -1,33 +1,43 @@
 import Settings from '../Settings';
 
-const settings = new Settings();
-
 test('check settings 1', () => {
+  const settings = new Settings();
   const expected = [['theme', 'dark'], ['music', 'trance'], ['difficulty', 'easy']];
-  const received = settings.getUserSet('theme', 'dark');
+  const received = settings.getUserSet;
   expect(received).toEqual(expected);
 });
 
 test('check settings 2', () => {
+  const settings = new Settings('light');
   const expected = [['theme', 'light'], ['music', 'trance'], ['difficulty', 'easy']];
-  const received = settings.getUserSet('theme', 'light');
+  const received = settings.getUserSet;
   expect(received).toEqual(expected);
 });
 
 test('check settings 3', () => {
+  const settings = new Settings('light', 'pop');
   const expected = [['theme', 'light'], ['music', 'pop'], ['difficulty', 'easy']];
-  const received = settings.getUserSet('music', 'pop');
+  const received = settings.getUserSet;
   expect(received).toEqual(expected);
 });
 
 test('check settings 4', () => {
-  const expected = [['theme', 'light'], ['music', 'pop'], ['difficulty', 'hard']];
-  const received = settings.getUserSet('difficulty', 'hard');
+  const settings = new Settings('light', 'house', 'hard');
+  const expected = [['theme', 'light'], ['music', 'trance'], ['difficulty', 'hard']];
+  const received = settings.getUserSet;
   expect(received).toEqual(expected);
 });
 
 test('check settings 5', () => {
-  const expected = 'Wrong data!';
-  const received = settings.getUserSet('difficuly', 'hard');
+  const settings = new Settings('grey', 'you will die');
+  const expected = [['theme', 'grey'], ['music', 'trance'], ['difficulty', 'easy']];
+  const received = settings.getUserSet;
+  expect(received).toEqual(expected);
+});
+
+test('check settings 6', () => {
+  const settings = new Settings('hard');
+  const expected = [['theme', 'dark'], ['music', 'trance'], ['difficulty', 'hard']];
+  const received = settings.getUserSet;
   expect(received).toEqual(expected);
 });
